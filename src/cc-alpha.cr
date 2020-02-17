fa198c25420db35de13addd8539b42fe0ceeb204
Crystal Danger <cdneal091@gmail.com>
2020-02-17 15:00:37 -0500

    fixed naming problem causing require issue

--- src/cc-alpha.cr
+++ src/cc-alpha.cr
@@ -1,10 +1,10 @@
 # TODO: Write documentation for `Alpha`
 require "http/web_socket"
 require "json"
-require "./alpha/*"
+require "./cc-alpha/*"
 
 module Alpha
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
   def self.test
     puts "test"
   end
