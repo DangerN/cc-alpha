1fd2619f4e1e60ec06ca361f6d55ac7e9ae35e2c
Crystal Danger <cdneal091@gmail.com>
2020-02-17 14:09:13 -0500

    first release

--- src/alpha.cr
+++ src/alpha.cr
@@ -5,10 +5,4 @@ require "./alpha/*"
 
 module Alpha
   VERSION = "0.1.0"
-  @@subscriber
-  # TODO: Put your code here
 end
-
-yeed = Alpha::Thread.from_json(%({"id":0,"flags":[""],"posts":[{"id":0,"badges":["peen"],"flags":[""]}]}))
-yeed.add_post(%({"id":1,"badges":["peen"],"flags":[""]}))
-puts yeed.posts
--- src/alpha/board.cr
+++ src/alpha/board.cr
@@ -7,5 +7,10 @@ module Alpha
       thread_limit: {type: UInt8, default: 25.to_u8},
       threads: Array(Thread)
     )
+
+    # Creates a `Thread` and add it to `Board`'s thread list.
+    def add_thread(thread_json)
+      @threads << Post.from_json(thread_json)
+    end
   end
 end
