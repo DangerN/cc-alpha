3c7db28766edd3b5cbdfb37ced4d495160762e74
Crystal Danger <cdneal091@gmail.com>
2020-02-17 16:34:02 -0500

    ready to use

--- src/cc-alpha.cr
+++ src/cc-alpha.cr
@@ -4,8 +4,37 @@ require "json"
 require "./cc-alpha/*"
 
 module Alpha
-  VERSION = "0.1.2"
-  def self.test
-    puts "test"
+  VERSION = "0.1.3"
+  @@boards = {} of String => Board
+
+  def self.boards
+    @@boards
   end
+
+  # Create a hash representation of all board ids names and flags.
+  def self.board_list
+    board_list = {} of String => Hash(String, String | Array(String))
+    @@boards.keys.map do |board|
+      board_list[board] = {"name" => @@boards[board].name, "flags" => @@boards[board].flags}
+    end
+    board_list
+  end
+
+end
+
+def board_by_id(board_id)
+  Alpha.boards[board_id]
+end
+
+def add_board_from_json(board_json)
+  new_board = Alpha::Board.from_json(board_json)
+  Alpha.boards[new_board.id] = new_board
+end
+
+def board_list
+  Alpha.board_list
+end
+
+def test_board
+  add_board_from_json(%({"id":"test","name":"Test","flags":[],"threads":{}}))
 end
--- src/cc-alpha/board.cr
+++ src/cc-alpha/board.cr
@@ -5,12 +5,13 @@ module Alpha
       name: String,
       flags: Array(String),
       thread_limit: {type: UInt8, default: 25.to_u8},
-      threads: Array(Thread)
+      threads: Hash(String, Thread)
     )
 
     # Creates a `Thread` and add it to `Board`'s thread list.
     def add_thread(thread_json)
-      @threads << Post.from_json(thread_json)
+      new_thread = Thread.from_json(thread_json)
+      @threads[new_thread.id.to_s] = new_thread
     end
   end
 end
